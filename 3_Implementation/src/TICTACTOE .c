c51416b58f111c8025df7ecf012edf74e7cb57e8
Hrishikesh Anand <sreerishi257@gmail.com>
2022-02-11 15:34:11 +0530

    Merge branch 'main' of https://github.com/Hrishikesh-Anand-07/M1_TicTacToe_Game

--- 3_Implementation/src/TICTACTOE .c
+++ 3_Implementation/src/TICTACTOE .c
@@ -1,5 +1,3 @@
-// Reference from Geeksforgeeks Link --> https://www.geeksforgeeks.org/implementation-of-tic-tac-toe-game/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "TICTACTOE.h"
@@ -61,35 +59,3 @@ int main()
     return 0;
 }
 
-int check()
-{
-    if (s[1] == s[2] && s[2] == s[3])
-        return 1;
-        
-    else if (s[4] == s[5] && s[5] == s[6])
-        return 1;
-        
-    else if (s[7] == s[8] && s[8] == s[9])
-        return 1;
-        
-    else if (s[1] == s[4] && s[4] == s[7])
-        return 1;
-        
-    else if (s[2] == s[5] && s[5] == s[8])
-        return 1;
-        
-    else if (s[3] == s[6] && s[6] == s[9])
-        return 1;
-        
-    else if (s[1] == s[5] && s[5] == s[9])
-        return 1;
-        
-    else if (s[3] == s[5] && s[5] == s[7])
-        return 1;
-        
-    else if (s[1] != '1' && s[2] != '2' && s[3] != '3' && s[4] != '4' && 
-        s[5]!='5' && s[6] != '6' && s[7] != '7' && s[8] != '8' && s[9] != '9')
-        return 0;
-    else
-        return  - 1;
-}
